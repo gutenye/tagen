44a4e1d05e47068a34982db321f76be22da37183
Guten <ywzhaifei@gmail.com>
2012-08-27 12:10:24 +0800

    add Numeric#humanize_time

--- lib/tagen/core/time.rb
+++ lib/tagen/core/time.rb
@@ -11,4 +11,32 @@ class Time
   end
 end
 
+class Numeric
+  # Reports the approximate distance in time between two Time, Date or DateTime objects or integers as seconds.
+  #
+  # @example
+  #
+  #   1.time_humanize(true)    -> 1 seconds
+  #   36561906.time_humanize   -> 1 years 2 months 3 days 4 hours 5 minutes
+  #
+  def time_humanize(include_seconds=false)
+    deta = self
+    deta, seconds = deta.divmod(60)
+    deta, minutes = deta.divmod(60)
+    deta, hours = deta.divmod(24)
+    deta, days = deta.divmod(30)
+    years, months = deta.divmod(12)
+
+    ret = ""
+    ret << "#{years} years " unless years == 0
+    ret << "#{months} months " unless months == 0
+    ret << "#{days} days " unless days == 0
+    ret << "#{hours} hours " unless hours == 0
+    ret << "#{minutes} minutes " unless minutes == 0
+    ret << "#{seconds} seconds" if include_seconds
+
+    ret.rstrip
+  end
+end
+
 require "active_support/time"
--- spec/tagen/core/time_spec.rb
+++ spec/tagen/core/time_spec.rb
@@ -8,3 +8,12 @@ describe Time do
     end
   end
 end
+
+describe Numeric do
+  describe "#time_humanize" do
+    it do
+      expect(36561906.time_humanize).to eq("1 years 2 months 3 days 4 hours 5 minutes")
+      expect(0.time_humanize(true)).to eq("0 seconds")
+    end
+  end
+end
